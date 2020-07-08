fc1468f0a1e28933819026584508ec5572bef4a7
alexgirel <alexandra.girel@gmail.com>
2020-07-08 22:46:42 +0200

    Correction fonctionnement boutons

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -13,27 +13,6 @@ export class PostListComponent implements OnInit {
     new Post("Mon premier post", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",  0 ),
     new Post("Mon deuxième post", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",  5 ),
     new Post("Mon troisième post", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",  -4 )
-    /*
-  ];
-  
-     /* titrePost: 'Mon premier post',
-      contenuPost: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      loveIts: 0,
-      /*created_at: ???*/
-    /*},
-    {
-      titre: 'Mon deuxième post',
-      contenu: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      loveIts: 1
-      /*created_at: */
-    /*},
-    {
-      titre: 'Mon troisième post',
-      contenu: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      loveIts: -1
-      /*created_at: */
-   /* }*/
-
   ];
 
   constructor() { }
