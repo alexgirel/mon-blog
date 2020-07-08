cf9b3ac312073fc79964f80e81f16dc05b06f61c
alexgirel <alexandra.girel@gmail.com>
2020-07-08 14:29:06 +0200

    Modification du tableau dans post-list

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -8,27 +8,27 @@ import { PostListItemComponent } from '../post-list-item/post-list-item.componen
 })
 export class PostListComponent implements OnInit {
   
-  listPosts: Post [];
-
-  /*  {
+  listPosts: Post [] = [
+    {
       titrePost: 'Mon premier post',
       contenuPost: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      loveIts: 0
-      created_at: new Date()
+      loveIts: 0,
+      /*created_at: ???*/
     },
     {
       titre: 'Mon deuxième post',
       contenu: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       loveIts: 1
-      created_at: new Date()
+      /*created_at: */
     },
     {
       titre: 'Mon troisième post',
       contenu: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       loveIts: -1
-      created_at: new Date()
+      /*created_at: */
     }
-  ];*/
+
+  ];
 
   constructor() { }
 
